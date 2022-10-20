ed0a4355fd8c35cb43d04fe111f9f3fd8c323484
Burhanuddin Mustafa Lakdawala <burhan123786@gmail.com>
2022-10-20 11:20:39 -0700

    startup.js -> .ts

--- src/hack/startup.ts
+++ src/hack/startup.ts
@@ -1,10 +1,16 @@
+import { NS } from "@ns";
+
 const exec_script = "/tools/exec.js";
 const hack_script = "/hack/just_hack.js";
 const grow_script = "/hack/just_grow.js";
 const weaken_script = "/hack/just_weaken.js";
 
-/** @param {import("../../").NS} ns */
-export async function main(ns) {
+/** @param {NS} ns */
+export async function main(ns: NS) {
+	if (ns.getHackingLevel() < 20) {
+		ns.alert("Level up to hacking level 20 first.");
+	}
+
 	ns.exec(exec_script, "home", 1, "foodnstuff", "all", grow_script, "n00dles");
 	ns.exec(exec_script, "home", 1, "nectar-net", "all", weaken_script, "n00dles");
 	ns.exec(exec_script, "home", 1, "n00dles", "all", hack_script, "n00dles");
