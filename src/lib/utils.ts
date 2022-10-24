ea0bb8f164a1676b5d3a81e4966dbcc1d638d111
Burhanuddin Mustafa Lakdawala <burhan123786@gmail.com>
2022-10-24 13:08:28 -0700

    spider_nuke: .ts conversion + common nuker() moved to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -71,3 +71,58 @@ export function getServerList(ns: NS) {
 	recurse(ns, 'home');
 	return server_list;
 }
+
+/** 
+ * @param {NS} ns 
+ * @param {string} server
+*/
+export function nukeServer(ns: NS, server: string) {
+	if (ns.hasRootAccess(server)) {
+		ns.print(`Already have root on ${server}`);
+		return;
+	}
+	if (ns.getServerRequiredHackingLevel(server) > ns.getHackingLevel()) {
+		ns.print(`Too hard to hack ${server} ` + 
+					`(${ns.getHackingLevel()} / ${ns.getServerRequiredHackingLevel(server)})`);
+		return;
+	}
+
+	let ports_required = ns.getServerNumPortsRequired(server);	
+
+	// Brute SSH if necessary
+	if (ports_required > 0 && ns.fileExists("BruteSSH.exe", "home")) {
+		ns.brutessh(server);
+		ports_required -= 1;			
+	}
+	// Crack FTP if necessary
+	if (ports_required > 0 && ns.fileExists("FTPCrack.exe", "home")) {
+		ns.ftpcrack(server);
+		ports_required -= 1;			
+	}
+	// Relay SMTP if necessary
+	if (ports_required > 0 && ns.fileExists("relaySMTP.exe", "home")) {
+		ns.relaysmtp(server);
+		ports_required -= 1;			
+	}
+	// Worm HTTP if necessary
+	if (ports_required > 0 && ns.fileExists("HTTPWorm.exe", "home")) {
+		ns.httpworm(server);
+		ports_required -= 1;			
+	}
+	// Inject SQL if necessary
+	if (ports_required > 0 && ns.fileExists("SQLInject.exe", "home")) {
+		ns.sqlinject(server);
+		ports_required -= 1;			
+	}
+
+	// is nuke possible?
+	if (ports_required > 0) {
+		ns.print(`More ports required for server ${server}`);
+		return;
+	}
+
+	// nuke
+	ns.nuke(server);
+	ns.print(`Nuked ${server}!`);
+	ns.tprint(`Nuked ${server}!`);
+}
