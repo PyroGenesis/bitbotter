43aa06b477b83a5ddd4c8d0b42a32279cefd3662
Burhanuddin Mustafa Lakdawala <burhan123786@gmail.com>
2022-10-25 15:24:11 -0700

    preparer_home: Only move on to next server when current server is finished

--- src/hack/preparer_home.ts
+++ src/hack/preparer_home.ts
@@ -71,46 +71,67 @@ export async function main(ns: NS) {
 		// let min_security = ns.getServerMinSecurityLevel(server.name);
 		// let max_money = ns.getServerMaxMoney(server.name);
 
-		const weaken_time = ns.getWeakenTime(server.name);
-		let finish_at = weaken_time;
+		// const weaken_time = ns.getWeakenTime(server.name);
+		let finish_at = 0; //weaken_time;
 
 		// weaken first
-		if (ns.getServerSecurityLevel(server.name) > server.min_security) {
+		while (true) {
+			// if security level is already lowest, we break
+			if (ns.getServerSecurityLevel(server.name) === server.min_security) break;
+			
 			// threads needed to attain min security
-			let weak_threads_needed = Math.ceil((ns.getServerSecurityLevel(server.name) - server.min_security) / 0.05);
+			const weak_threads_needed = Math.ceil((ns.getServerSecurityLevel(server.name) - server.min_security) / 0.05);
 			// threads we can make currently
 			let weak_threads_possible_curr = Math.floor(getServerAvailableRam(ns) / 1.75);
 			// threads we can make in ideal scenario (only this script running)
 			const weak_threads_possible_max = Math.floor((ns.getServer().maxRam - ns.getScriptRam(ns.getScriptName())) / 1.75);
-			// let weak_threads = Math.min(weak_threads_needed, weak_threads_possible_curr);
 
-			// lower threads needed if just not possible
-			weak_threads_needed = Math.min(weak_threads_needed, weak_threads_possible_max);
+			// whether we have enough RAM to fulfill all weakens needed right now
+			let possible_now = weak_threads_needed <= weak_threads_possible_curr;
+			// whether we will have enough RAM to fulfill all weakens needed, but don't have enough right now
+			const possible_later = weak_threads_needed <= weak_threads_possible_max;
+			// we do not have enough RAM to fulfill all weakens needed, not even later, so we do the weakens in batches
+			const need_to_repeat = !possible_later;
 
-			// if we cannot make enough threads right now (but can do so later),
-			//	wait for RAM to get free
-			if (weak_threads_possible_curr < weak_threads_needed) {
-				ns.tprint("Ran out of RAM");
+			const weaken_time = ns.getWeakenTime(server.name);
+
+			if (possible_now) {
+				// launch all needed weaken threads
+				ns.tprint(`Running ${weak_threads_needed} weakens for ${server.name}`);
+				ns.exec(weaken_script, host_server, weak_threads_needed, server.name, 0, id, 20);
+
+				finish_at = weaken_time + 10000;
+				break;
+
+			} else if (need_to_repeat) {
+				// launch all possible weaken threads, we will do more later
+				ns.tprint(`Running ${weak_threads_possible_curr} weakens for ${server.name}. Will repeat for more.`);
+				ns.exec(weaken_script, host_server, weak_threads_possible_curr, server.name, 0, id, 20);
+
+				await ns.sleep(weaken_time + 1000);
+
+			} else if (possible_later) {
 				// wait until enough RAM is freed
-				while (weak_threads_possible_curr < weak_threads_needed) {
+				ns.tprint(`Not enough RAM for weakens on ${server.name}. Will wait until available.`);
+				while (!possible_now) {
 					await ns.sleep(10000);
 					// recalc
 					weak_threads_possible_curr = Math.floor((ns.getServer().maxRam - ns.getScriptRam(ns.getScriptName())) / 1.75);
+					possible_now = weak_threads_needed <= weak_threads_possible_curr;
 				}
 			}
-
-			ns.tprint(`Running ${weak_threads_needed} weakens for ${server.name}`);
-			ns.exec(weaken_script, host_server, weak_threads_needed, server.name, 0, id, 20);
-
-			finish_at += 10000;
 		}
 		
+		
 		// then grow-weaken
-		if (ns.getServerMoneyAvailable(server.name) < server.max_money) {
+		while (true) {
+			// if money is already max, we break
+			if (ns.getServerMoneyAvailable(server.name) === server.max_money) break;
+
 			// grow weaken batch
 			const grow_multipler_required = server.max_money / ns.getServerMoneyAvailable(server.name);
 			// batches needed to attain 100% cash
-			let batches_needed = Math.ceil(ns.growthAnalyze(server.name, grow_multipler_required, ns.getServer().cpuCores));
+			const batches_needed = Math.ceil(ns.growthAnalyze(server.name, grow_multipler_required, ns.getServer().cpuCores) / GROW_PER_WEAKEN);
 
 			const jobs = GROW_PER_WEAKEN + 1
 			const ram_per_batch = jobs * 1.75;
@@ -119,25 +140,42 @@ export async function main(ns: NS) {
 			// batches we can make in ideal scenario (only this script running)
 			const batches_possible_max = Math.floor((ns.getServer().maxRam - ns.getScriptRam(ns.getScriptName())) / ram_per_batch);
 
-			// lower batches needed if just not possible
-			batches_needed = Math.min(batches_needed, batches_possible_max);
+			// whether we have enough RAM to fulfill all batches needed right now
+			let possible_now = batches_needed <= batches_possible_curr;
+			// whether we will have enough RAM to fulfill all batches needed, but don't have enough right now
+			const possible_later = batches_needed <= batches_possible_max;
+			// we do not have enough RAM to fulfill all batches needed, not even later, so we do the batches in batches
+			const need_to_repeat = !possible_later;
 
-			// if we cannot make enough batches right now (but can do so later),
-			//	wait for RAM to get free
-			if (batches_possible_curr < batches_needed) {
-				ns.tprint("Ran out of RAM");
+			const weaken_time = ns.getWeakenTime(server.name);
+
+			if (possible_now) {
+				// launch all needed batches of grow-weaken
+				ns.tprint(`Running ${batches_needed} batches of grow-weaken for ${server.name}`);
+				ns.exec(grow_script, host_server, batches_needed * GROW_PER_WEAKEN, server.name, finish_at - ns.getGrowTime(server.name), id+1, 20);
+				finish_at += 10000;
+				ns.exec(weaken_script, host_server, batches_needed, server.name, finish_at - ns.getWeakenTime(server.name), id+1, 20);
+
+				break;
+			} else if (need_to_repeat) {
+				// launch all possible batches of grow-weaken, we will do more later
+				ns.tprint(`Running ${batches_possible_curr} batches of grow-weaken for ${server.name}. Will repeat for more.`);
+				ns.exec(grow_script, host_server, batches_possible_curr * GROW_PER_WEAKEN, server.name, finish_at - ns.getGrowTime(server.name), id+1, 20);
+				finish_at += 10000;
+				ns.exec(weaken_script, host_server, batches_possible_curr, server.name, finish_at - ns.getWeakenTime(server.name), id+1, 20);
+
+				await ns.sleep(weaken_time + 1000);
+
+			} else if (possible_later) {
 				// wait until enough RAM is freed
-				while (batches_possible_curr < batches_needed) {
+				ns.tprint(`Not enough RAM for batches of grow-weaken on ${server.name}. Will wait until available.`);
+				while (!possible_now) {
 					await ns.sleep(10000);
 					// recalc
 					batches_possible_curr = Math.floor(getServerAvailableRam(ns) / ram_per_batch);
+					possible_now = batches_needed <= batches_possible_curr;
 				}
 			}
-			
-			ns.tprint(`Running ${batches_needed} batches of grow-weaken for ${server.name}`);
-			ns.exec(grow_script, host_server, batches_needed * GROW_PER_WEAKEN, server.name, finish_at - ns.getGrowTime(server.name), id+1, 20);
-			finish_at += 10000;
-			ns.exec(weaken_script, host_server, batches_needed, server.name, finish_at - ns.getWeakenTime(server.name), id+1, 20);
 		}
 
 		id += 2;	
